670f9ffa79035de2845522b4d03e397d66f2cdbd
Gregory Chebatyuk <gchebatyuk@gmail.com>
2025-03-07 14:28:16 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 // import ReactLogo from './assets/react.svg?react';
 // import viteLogo from '/vite.svg';
 import styles from './app.module.css';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
+	const [toDo, setToDo] = useState([]);
 	return <div className={styles.App}></div>;
 };
